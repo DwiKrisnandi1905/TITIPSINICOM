ad61c1caed6cfd72e31dc4ffa346c07b32697cd5
febriann <benigiovanni3@gmail.com>
2023-08-24 06:46:52 +0700

    landing page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,14 +3,7 @@
 @section('content')
 <div class="row">
     <div class="col-md-6 col-lg-6 mb-2">
-        <p style="color: #000; font-family: Poppins; font-size: 36px; font-style: normal; font-weight: 500; line-height: normal; margin-left: 70px">Bingung mau nitip barang dimana?</p>
-        <p style="color: #000; font-family: Poppins; font-size: 24px; font-style: normal; font-weight: 500; line-height: normal; margin-bottom: 40px; margin-left: 70px">Titip sini aja!</p>
-        <p style="color: #000; font-family: Poppins; font-size: 18px; font-style: normal; font-weight: 400; line-height: normal; margin-bottom: 20px; margin-left: 70px">Semua bisa dititipkan termasuk barang, rumah atau kendaraan.</p>
-        <a class="badge badge-edit text-white bg-primary rounded-pill d-inline" href="#" style="text-decoration: none; margin-left: 70px; display: flex; width: 300px; padding: 10px;">Titip sekarang!</a>
-        <div class="row">
-            
-            <p style="color: #000; font-family: Poppins; font-size: 18px; font-style: normal; font-weight: 500; line-height: normal; margin-bottom: 40px; margin-left: 70px; margin-top: 40px">Melayani 24 jam.</p>
-        </div>
+        
     </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;700&display=swap">
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet" />
     <link rel="icon" href="{{ asset('/img/logo1.png') }}" type="image/png">
     <title>@yield('title', 'Online Store')</title>
@@ -12,19 +13,24 @@
 
 <body>
     <!-- header -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary py-4">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary py-4 border-bottom">
         <div class="container">
-            <img src="{{ asset('/img/logo1.png') }}" class="img-title">
-            <a class="navbar-brand" style="color: #262D33; font-size: 24px; font-family: Poppins; font-weight: 600; word-wrap: break-word; text-decoration: none;" href="#">Titipsini.com</a>
+            <div class="col-md-6 col-lg-6">
+                <img src="{{ asset('/img/logo1.png') }}" class="img-title">
+                
+            </div>
+            <!-- <a class="navbar-brand" style="color: #262D33; font-size: 24px; font-family: Raleway; font-weight: 600; word-wrap: break-word; text-decoration: none;" href="/">Titipsini.com</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
-            </button>
+            </button> -->
+            
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav ms-auto" style="display: inline-flex; align-items: flex-start; gap: 52px;">
-                    <a class="nav-link active" style="color: #000; font-family: Poppins; font-size: 18px; font-style: normal; font-weight: 500; line-height: normal;" href="/">Home</a>
-                    <a class="nav-link active" style="color: #000; font-family: Poppins; font-size: 18px; font-style: normal; font-weight: 500; line-height: normal;" href="/about">About</a>
-                    <a class="nav-link active" style="color: #000; font-family: Poppins; font-size: 18px; font-style: normal; font-weight: 500; line-height: normal;" href="#">Service</a>
-                    <a class="nav-link active" style="color: #000; font-family: Poppins; font-size: 18px; font-style: normal; font-weight: 500; line-height: normal;" href="#">Contact Us</a>
+                    <a class="nav-link active" style="color: #000; font-family: Raleway; font-size: 18px; font-style: normal; font-weight: 500; line-height: normal;" href="/">Beranda</a>
+                    <a class="nav-link active" style="color: #000; font-family: Raleway; font-size: 18px; font-style: normal; font-weight: 500; line-height: normal;" href="/about">Tentang</a>
+                    <a class="nav-link active" style="color: #000; font-family: Raleway; font-size: 18px; font-style: normal; font-weight: 500; line-height: normal;" href="#">Layanan</a>
+                    <a class="nav-link active" style="color: #000; font-family: Raleway; font-size: 18px; font-style: normal; font-weight: 500; line-height: normal;" href="#">FAQ</a>
+                    <a href="#"><img src="{{ asset('/img/User_fill.png') }}" class="user-login"></a>
                 </div>
             </div>
         </div>
