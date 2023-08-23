fae0352355659272674998b0c4c43a1e40be3dde
febriann <benigiovanni3@gmail.com>
2023-08-23 09:38:40 +0700

    navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,14 +13,17 @@
     <!-- header -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-secondary py-4">
         <div class="container">
-            <a class="navbar-brand title" href="#">Titipsini.com</a>
+            <img src="{{ asset('/img/logo1.png') }}" class="img-title">
+            <a class="navbar-brand" style="color: #262D33; font-size: 36px; font-family: Poppins; font-weight: 600; word-wrap: break-word; text-decoration: none;" href="#">Titipsini.com</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav ms-auto">
-                    <a class="nav-link active" href="#">Home</a>
-                    <a class="nav-link active" href="#">About</a>
+                <div class="navbar-nav ms-auto" style="display: inline-flex; align-items: flex-start; gap: 52px;">
+                    <a class="nav-link active" style="color: #000; font-family: Poppins; font-size: 24px; font-style: normal; font-weight: 500; line-height: normal;" href="#">Home</a>
+                    <a class="nav-link active" style="color: #000; font-family: Poppins; font-size: 24px; font-style: normal; font-weight: 500; line-height: normal;" href="#">About</a>
+                    <a class="nav-link active" style="color: #000; font-family: Poppins; font-size: 24px; font-style: normal; font-weight: 500; line-height: normal;" href="#">Service</a>
+                    <a class="nav-link active" style="color: #000; font-family: Poppins; font-size: 24px; font-style: normal; font-weight: 500; line-height: normal;" href="#">Contact Us</a>
                 </div>
             </div>
         </div>
