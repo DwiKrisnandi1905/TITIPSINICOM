88a9870e6b50b2a27f97d455e6437ef3b2a4d477
DwiKrisnandi1905 <116879948+DwiKrisnandi1905@users.noreply.github.com>
2023-08-24 09:18:08 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Route::get('/', function () {
     return view('home.index')->with("viewData", $viewData);
 });
 
-Route::get('/about', 'App\Http\Controllers\HomeController@about')->name("home.about"); 
+Route::get('/about', 'App\Http\Controllers\HomeController@about')->name("home.about");
