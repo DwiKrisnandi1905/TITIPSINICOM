51e7a5199a35e242e2d8d78e5ed86f84411fb8a2
febriann <benigiovanni3@gmail.com>
2023-08-28 15:27:20 +0700

    Update login.blade.php

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,108 +1,118 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Login Card</title>
-<style>
-  body {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    min-height: 100vh;
-    margin: 0;
-    background-color: #f0f0f0;
-  }
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login Card</title>
+  <style>
+    body {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      min-height: 100vh;
+      margin: 0;
+      background-color: #f0f0f0;
+    }
 
-  .card {
-    width: 195px;
-    padding: 100px;
-    background-color: white;
-    border: 1px solid black;
-    border-radius: 10px;
-    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.5);
-    margin-left: 500px;
-  }
-  .card-header h1 {
-    font-size: 24px;
-    font-weight: bold;
-    margin-bottom: 50px;
-    margin-left: -40px;
-    margin-top: -50px;
-  }
-  .form-group {
-    margin-bottom: 30px;
-    margin-left: -40px;
-    margin-right: -30px;
-  }
+    .card {
+      width: 195px;
+      padding: 100px;
+      background-color: white;
+      border: 1px solid black;
+      border-radius: 10px;
+      box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.5);
+      margin-left: 500px;
+    }
 
-  .form-label {
-    display: block;
-    margin-bottom: 5px;
-    font-weight: bold;
-  }
-  .form-label img {
-        position: relative;
+    .card-header h1 {
+      font-size: 24px;
+      font-weight: bold;
+      margin-bottom: 50px;
+      margin-left: -40px;
+      margin-top: -50px;
+    }
+
+    .form-group {
+      margin-bottom: 30px;
+      margin-left: -40px;
+      margin-right: -30px;
+    }
+
+    .form-label {
+      display: block;
+      margin-bottom: 5px;
+      font-weight: bold;
+    }
+
+    .form-label img {
+      position: relative;
     }
 
     #password-toggle {
-        position: absolute;
-        top: 335px;
-        right: 235px;
-        transform: translateY(-50%);
-        cursor: pointer;
-        width: 20px; /* Sesuaikan ukuran gambar */
-        height: 20px; /* Sesuaikan ukuran gambar */
-        background-image: url('/img/open.png'); /* Menggunakan sprite sebagai gambar latar belakang */
-        background-position: 0 0; /* Mengatur posisi sprite (mata terbuka) */
+      position: absolute;
+      top: 335px;
+      right: 235px;
+      transform: translateY(-50%);
+      cursor: pointer;
+      width: 20px;
+      /* Sesuaikan ukuran gambar */
+      height: 20px;
+      /* Sesuaikan ukuran gambar */
+      background-image: url('/img/open.png');
+      /* Menggunakan sprite sebagai gambar latar belakang */
+      background-position: 0 0;
+      /* Mengatur posisi sprite (mata terbuka) */
     }
 
     #password-toggle.cross {
-        background-position: -20px 0; /* Mengatur posisi sprite (silang) */
+      background-position: -20px 0;
+      /* Mengatur posisi sprite (silang) */
     }
 
-  .form-input {
-    width: 100%;
-    padding: 8px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-  }
+    .form-input {
+      width: 100%;
+      padding: 8px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+    }
 
 
-  .form-button {
-    width: 282px;
-    padding: 10px;
-    background-color: #068113;
-    color: white;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    margin-left: -40px;
+    .form-button {
+      width: 282px;
+      padding: 10px;
+      background-color: #068113;
+      color: white;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+      margin-left: -40px;
 
-  }
+    }
 
-  .form-button:hover {
-    background-color: #0056b3;
-  }
-</style>
+    .form-button:hover {
+      background-color: #0056b3;
+    }
+  </style>
 </head>
+
 <body>
   <div class="card">
     <div class="card-header">
-        <h1>Login Admin</h1>
-        </div>
+      <h1>Login Admin</h1>
+    </div>
     <form>
       <div class="form-group">
         <label for="email" class="form-label">Email:</label>
         <input type="email" id="email" class="form-input" required>
       </div>
       <div class="form-group">
-    <label for="password" class="form-label">
-        Kata Sandi:
-        <img src="{{ asset('/img/open.png') }}" alt="Tampilkan/Sembunyikan Sandi" id="password-toggle">
-    </label>
-    <input type="password" id="password" class="form-input" required>
-</div>
+        <label for="password" class="form-label">
+          Kata Sandi:
+          <img src="{{ asset('/img/open.png') }}" alt="Tampilkan/Sembunyikan Sandi" id="password-toggle">
+        </label>
+        <input type="password" id="password" class="form-input" required>
+      </div>
 
       <button type="submit" class="form-button">Login</button>
     </form>
@@ -114,12 +124,11 @@
     let passwordVisible = false;
 
     passwordToggle.addEventListener('click', function() {
-        passwordVisible = !passwordVisible;
-        passwordInput.type = passwordVisible ? 'text' : 'password';
-        passwordToggle.classList.toggle('cross', passwordVisible);
+      passwordVisible = !passwordVisible;
+      passwordInput.type = passwordVisible ? 'text' : 'password';
+      passwordToggle.classList.toggle('cross', passwordVisible);
     });
-</script>
-
-
+  </script>
 </body>
+
 </html>
