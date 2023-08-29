fd4c9767b253461e0fbda9f5dc48deead4454a03
febriann <benigiovanni3@gmail.com>
2023-08-29 15:50:52 +0700

    footer navbar responsive

--- resources/views/components/guest/footer.blade.php
+++ resources/views/components/guest/footer.blade.php
@@ -17,41 +17,41 @@
             <div class="col-md">
                 <ul class="list-unstyled nav-links">
                     <li>
-                        <p
+                        <p class="footer-title"
                             style="font-family: Poppins; font-size: 18px; font-style: normal; font-weight: 500; line-height: normal; color: #fff">
                             Produk</p>
                     </li>
-                    <li><a href="{{ route('guest.syarat-ketentuan') }}">Syarat & Ketentuan</a></li>
-                    <li><a href="{{ route('guest.privacy-policy') }}">Kebijakan Privasi</a></li>
-                    <li><a href="{{ route('guest.faq') }}">FAQ</a></li>
+                    <li><a class="footer-title" href="{{ route('guest.syarat-ketentuan') }}">Syarat & Ketentuan</a></li>
+                    <li><a class="footer-title" href="{{ route('guest.privacy-policy') }}">Kebijakan Privasi</a></li>
+                    <li><a class="footer-title" href="{{ route('guest.faq') }}">FAQ</a></li>
                 </ul>
             </div>
             <div class="col-md">
                 <ul class="list-unstyled nav-links">
                     <li>
-                        <p
+                        <p class="footer-title"
                             style="font-family: Poppins; font-size: 18px; font-style: normal; font-weight: 500; line-height: normal; color: #fff;">
                             Perusahaan</p>
                     </li>
-                    <li><a href="{{ route('guest.about') }}">Tentang Kami</a></li>
-                    <li><a href="{{ route('guest.contact') }}">Kontak Kami</a></li>
+                    <li><a class="footer-title" href="{{ route('guest.about') }}">Tentang Kami</a></li>
+                    <li><a class="footer-title" href="{{ route('guest.contact') }}">Kontak Kami</a></li>
                 </ul>
             </div>
             <div class="col-md">
                 <ul class="list-unstyled nav-links">
-                    <div class="row">
+                    <div class="row footer-title">
                         <li>
                             <i class="bi bi-facebook" style="margin-right: 10px;"></i>
                             <a href="#">Facebook</a>
                         </li>
                     </div>
-                    <div class="row">
+                    <div class="row footer-title">
                         <li>
                             <i class="bi bi-twitter" style="margin-right: 10px;"></i>
                             <a href="#">Twitter</a>
                         </li>
                     </div>
-                    <div class="row">
+                    <div class="row footer-title">
                         <li>
                             <i class="bi bi-instagram" style="margin-right: 10px;"></i>
                             <a href="#">Instagram</a>
@@ -59,13 +59,12 @@
                     </div>
                 </ul>
             </div>
-            <div class="col-md text-md-center">
-                <a href="#">
-                    <img src="{{ asset('/img/getplay.png') }}" alt="Google Play"
-                        style="margin-bottom: 20px; margin-left: -70px;">
+            <div class="col-md">
+                <a href="#" class="footer-app">
+                    <img src="{{ asset('/img/getplay.png') }}" alt="Google Play" style="width: 150px">
                 </a>
                 <a href="#">
-                    <img src="{{ asset('/img/getapps.png') }}" alt="Apple App" style="margin-left: -70px">
+                    <img src="{{ asset('/img/getapps.png') }}" alt="Apple App" style="width: 150px">
                 </a>
             </div>
         </div>
