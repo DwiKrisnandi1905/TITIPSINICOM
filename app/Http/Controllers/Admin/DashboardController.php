198b4a480e931e7cb084fe44943d3b372e3e65a9
febriann <benigiovanni3@gmail.com>
2023-08-30 17:32:05 +0700

    dashboard admin

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -10,7 +10,7 @@ class DashboardController extends Controller
     public function dashboard()
     {
         return view('admin.dashboard', [
-            'title' => 'dashboard'
+            'title' => 'Dashboard'
         ]);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,1 +1,25 @@
-@extends('components.dashboard.navbar')
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;700&display=swap">
+    <link href="{{ asset('/css/admin.css') }}" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="icon" href="{{ asset('/img/logo1.png') }}" type="image/png">
+    <title>{{ $title }}</title>
+</head>
+<body>
+    <!-- sidebar -->
+    @include('components.dashboard.navbar')
+    <!-- sidebar -->
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
