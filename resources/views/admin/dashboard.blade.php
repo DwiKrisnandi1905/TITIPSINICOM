4eee05183c40616149efc262ccbd610af54723eb
febriann <benigiovanni3@gmail.com>
2023-08-31 14:52:45 +0700

    .

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,8 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;700&display=swap">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
@@ -15,31 +14,35 @@
 </head>
 <style>
     .col-md-9 {
-    position: fixed;
-    top: 0;
-    left: 180px;
-    bottom: 0;
+        position: fixed;
+        top: 0;
+        left: 180px;
+        bottom: 0;
     }
 </style>
 
 <body>
 
-<div class="container mt-4">
-    <div class="row">
-        <div class="col-md-3">
-            @include('components.dashboard.sidebar')
-        </div>
-        <div class="col-md-9">
-            @yield('content')
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-md-3">
+                @include('components.dashboard.sidebar')
+            </div>
+            <div class="topbar sticky-top">
+                <span class="notification-icon">
+                    <i class="bi bi-bell"></i>
+                </span>
+            </div>
+            <div class="col-md-9">
+                @yield('content')
+            </div>
         </div>
     </div>
-</div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
     </script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
-        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script>
         // Logout Button
         $('.logout-button').click(function(event) {
