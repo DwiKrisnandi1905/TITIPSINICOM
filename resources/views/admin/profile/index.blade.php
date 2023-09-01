d071d238febf62f2f7df07a46b1d12a2242a2d3e
DwiKrisnandi1905 <116879948+DwiKrisnandi1905@users.noreply.github.com>
2023-09-01 15:20:39 +0700

    profil

--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.admin')
 @section('content')
+<style>
     <style>
         .profile-upload {
             text-align: center;
@@ -16,8 +17,8 @@
             border: 2px solid black;
             border-radius: 15%;
             overflow: hidden;
-            margin-top: 70px;
-            margin-left: -550px;
+            margin-top: 10px;
+            margin-left: 20px;
         }
 
         .layout-1 img {
@@ -80,15 +81,155 @@
             font-weight: 700;
             line-height: normal;
         }
-    </style>
-    <div class="profile-upload">
-        <div class="profile-title">Profil Pengguna</div>
-        <div class="profile-image layout-1">
-            <img src="{{ asset('/img/Logo.png') }}" alt="Profile Picture">
-            <div class="overlay">
-                <input type="file" id="profile-input" accept="image/*">
+        /*data user*/
+    .profile-info {
+        margin-top: 110px;
+        margin-left: -100px;
+    }
+    .profile-info h2 {
+        text-align: left;
+        color: #000;
+        font-family: Poppins;
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 600;
+        line-height: normal;
+    }
+
+    .form-group {
+        margin-bottom: 1px;
+    }
+
+    .form-group label {
+
+        color: var(--grey-900, #101828);
+        font-family: Raleway;
+        font-size: 14px;
+        font-style: normal;
+        font-weight: 600;
+        line-height: 160%;
+    }
+    .form-group input {
+        padding: 2px;
+    }
+
+    .form-control {
+        width: 100%;
+        padding: 10px;
+        border: 1px solid #ccc;
+        border-radius: 3px;
+        background: var(--grey-gray, #F0F4FD);
+        border-color: transparent;
+    }
+    .profile-picture h1 {
+        margin: 70px 150px;
+        color: #000;
+        font-family: Raleway;
+        font-size: 32px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: normal;
+    }
+    /*button*/
+    .buttonubah{
+        display: flex;
+        justify-content: center; /* Menengahkan secara horizontal */
+        align-items: center;
+        padding: 20px;
+
+    }
+    .ubah-button {
+    padding: 3px 80px;
+    background-color: #299935; /* Warna latar belakang */
+    color: #fff; /* Warna teks */
+    border: none;
+    border-radius: 10px;
+    cursor: pointer;
+    font-size: 16px;
+   
+    }
+
+    /* Gaya saat tombol dihover */
+    .ubah-button:hover {
+        background-color: #0056b3; /* Warna latar belakang saat dihover */
+    }
+
+    /* Gaya saat tombol aktif (ditekan) */
+    .ubah-button:active {
+        background-color: #004b9e; /* Warna latar belakang saat ditekan */
+    }
+    .no-border input {
+        border: none;
+    }
+    .form-control1 {
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+    background-color: transparent;
+}
+
+</style>
+<div class="container-profile">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="profile-picture">
+                <!-- Masukkan gambar profil di sini -->
+                <h1>Admin Profile</h1>
+                <div class="profile-upload">
+                    <div class="profile-image layout-1">
+                        <img src="{{ asset('/img/Logo.png') }}" alt="Profile Picture">
+                    <div class="overlay">
+                    <input type="file" id="profile-input" accept="image/*">
                 <label class="label-upload" for="profile-input">upload</label>
             </div>
         </div>
     </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="profile-info">
+                <h2>Account ID:1
+                <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+                </h2>
+                <div class="form-group no-border">
+                    <label for="email">Email</label>
+                    <input type="text" class="form-control1" id="email" name="email" value="user@example.com" readonly>
+                </div>
+                <div class="form-group no-border">
+                    <label for="username">Username</label>
+                    <input type="text" class="form-control1" id="username" name="username" value="myusername" readonly>
+                </div>
+                <div class="form-group">
+                    <label for="nama">Nama Lengkap</label>
+                    <input type="text" class="form-control" id="nama lengkap" name="nama" value="John Doe" readonly>
+                </div>
+                <div class="form-group">
+                    <label for="nama">Alamat</label>
+                    <input type="text" class="form-control" id="alamat" name="nama" value="John Doe" readonly>
+                </div>
+                <div class="form-group">
+                    <label for="nama">Provinsi</label>
+                    <input type="text" class="form-control" id="Provinsi" name="nama" value="John Doe" readonly>
+                </div>
+                <div class="form-group">
+                    <label for="nama">Kabupaten/Kota</label>
+                    <input type="text" class="form-control" id="kab" name="nama" value="John Doe" readonly>
+                </div>
+                <div class="form-group">
+                    <label for="nama">Kecamatan</label>
+                    <input type="text" class="form-control" id="Kecamatan" name="nama" value="John Doe" readonly>
+                </div>
+                <div class="form-group">
+                    <label for="nama">Kode Pos</label>
+                    <input type="text" class="form-control" id="nama" name="nama" value="John Doe" readonly>
+                </div>
+                <div class="buttonubah">
+                    <button class="ubah-button">Ubah</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
+
