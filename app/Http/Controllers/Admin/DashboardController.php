90798cb3ec08103fbc235bd335ee690355f10562
Alfian57 <alfiangadingsaputra@gmail.com>
2023-09-01 09:40:15 +0700

    Finish dashboard backend

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,13 +3,62 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin;
+use App\Models\BuildingVendorService;
+use App\Models\CommodityVendorService;
+use App\Models\Customer;
+use App\Models\Finance;
+use App\Models\PickupVendorService;
+use App\Models\VehicleVendorService;
+use App\Models\Vendor;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        $counts = [
+            'customer' => Customer::class,
+            'vendor' => Vendor::class,
+            'finance' => Finance::class,
+            'admin' => Admin::class,
+            'building_vendor_service' => BuildingVendorService::class,
+            'pickup_vendor_service' => PickupVendorService::class,
+            'vehicle_vendor_service' => VehicleVendorService::class,
+            'commodity_vendor_service' => CommodityVendorService::class,
+        ];
+
+        $userCounts = [];
+
+        foreach ($counts as $key => $modelClass) {
+            $userCounts[$key] = $this->getUserCount($modelClass);
+        }
+
         return view('admin.dashboard.index', [
             'title' => 'Dashboard',
+            'userCount' => $userCounts['customer'] + $userCounts['vendor'] + $userCounts['finance'] + $userCounts['admin'],
+            'customerCount' => $userCounts['customer'],
+            'vendorCount' => $userCounts['vendor'],
+            'serviceCount' => $userCounts['building_vendor_service'] + $userCounts['pickup_vendor_service'] + $userCounts['vehicle_vendor_service'] + $userCounts['commodity_vendor_service'],
         ]);
+    }
+
+    private function getUserCount($modelClass)
+    {
+        return $modelClass::query()
+            ->when(request('month'), function ($query) {
+                $query->whereMonth('created_at', request('month'));
+            })
+            ->when(!request('month'), function ($query) {
+                $query->whereMonth('created_at', now());
+            })
+            ->when(request('year'), function ($query) {
+                $query->whereYear('created_at', request('year'));
+            })
+            ->when(!request('year'), function ($query) {
+                $query->whereYear('created_at', now());
+            })
+            ->count();
     }
 }
--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -3,7 +3,10 @@
     <div class="container mt-1 home">
         <div class="row">
             <div class="col">
-                <h2 style="margin-left: 28px; font-weight: 700;">Maret 2023</h2>
+                <h2 style="margin-left: 28px; font-weight: 700;">
+                    {{ request('month') ? date('F', mktime(0, 0, 0, request('month'), 10)) : date('F') }}
+                    {{ request('year') ?? date('Y') }}
+                </h2>
             </div>
             <div class="col text-end">
                 <div class="dropdown dropdown-end1">
@@ -12,9 +15,14 @@
                         Month
                     </button>
                     <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                        <li><a class="dropdown-item" href="#">March</a></li>
-                        <li><a class="dropdown-item" href="#">April</a></li>
-                        <li><a class="dropdown-item" href="#">May</a></li>
+                        @for ($i = 1; $i <= 12; $i++)
+                            <li>
+                                <a class="dropdown-item"
+                                    href="{{ route('admin.dashboard', ['month' => $i, 'year' => request('year')]) }}">
+                                    {{ date('F', mktime(0, 0, 0, $i, 10)) }}
+                                </a>
+                            </li>
+                        @endfor
                     </ul>
                 </div>
             </div>
@@ -25,9 +33,11 @@
                         Year
                     </button>
                     <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
-                        <li><a class="dropdown-item" href="#">2022</a></li>
-                        <li><a class="dropdown-item" href="#">2023</a></li>
-                        <li><a class="dropdown-item" href="#">2024</a></li>
+                        @for ($i = 0; $i < 5; $i++)
+                            <li><a class="dropdown-item"
+                                    href="{{ route('admin.dashboard', ['month' => request('month'), 'year' => date('Y') - $i]) }}">{{ date('Y') - $i }}</a>
+                            </li>
+                        @endfor
                     </ul>
                 </div>
             </div>
@@ -38,25 +48,25 @@
                 <div class="col-md-3">
                     <div class="container-total text-center">
                         <h2 style="font-size: 24px;">Total Pengguna</h2>
-                        <h1 class="text-total">20</h1>
+                        <h1 class="text-total">{{ $userCount }}</h1>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="container-total text-center">
                         <h2 style="font-size: 24px;">Total Customer</h2>
-                        <h1 class="text-total">20</h1>
+                        <h1 class="text-total">{{ $customerCount }}</h1>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="container-total text-center">
                         <h2 style="font-size: 24px;">Total Vendor</h2>
-                        <h1 class="text-total">20</h1>
+                        <h1 class="text-total">{{ $vendorCount }}</h1>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="container-total text-center">
                         <h2 style="font-size: 24px;">Total Layanan</h2>
-                        <h1 class="text-total">20</h1>
+                        <h1 class="text-total">{{ $serviceCount }}</h1>
                     </div>
                 </div>
             </div>
